021f65428f452cd275f07381489a82aa36dfb899
Nhat Le <symbol057vn@gmail.com>
2023-05-08 17:51:36 +0700

    identify

---
+++ EnglishForKids_LMN/Controllers/IdentifyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnglishForKids_LMN.Controllers
+{
+    public class IdentifyController : Controller
+    {
+        // GET: Identify
+        public ActionResult Identify_AI()
+        {
+            return View();
+        }
+    }
+}
